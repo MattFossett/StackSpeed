2df0884949aa0f130395e6c2e5aed7ea2bffde1f
Matt Fossett <mattfoss01@gmail.com>
2018-03-09 17:50:01 -0500

    first commit

---
+++ StackSpeed.cc
@@ -0,0 +1,21 @@
+/** 
+ * Author: Matt Fossett
+ * 
+ * Description: Test speeds of stacks using proper implementation versus poor implementation
+ * 
+ */
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <iterator>
+
+using std::cout;
+using std::cin;
+using std::vector;
+
+
+int
+main()
+{
+    cout << "Hello";
+}
